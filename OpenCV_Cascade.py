1a7d5aef78a780f230110574ea8e547a8fcba0cd
KenoLeon <me@k3no.com>
2020-08-04 16:26:36 -0500

    OPENCV_CASCADE

--- OpenCV_Cascade.py
+++ OpenCV_Cascade.py
@@ -8,14 +8,14 @@ while(True):
     # Convert to Greyscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Denoise (also try bluring, this is expensive )
-    frame = cv2.fastNlMeansDenoising(frame, h=3, templateWindowSize=7, searchWindowSize=21)
+    denoised = cv2.fastNlMeansDenoising(gray, h=3, templateWindowSize=7, searchWindowSize=21)
     # Apply threshold
-    frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+    thresholded = cv2.adaptiveThreshold(denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
     # Mirror image
-    frame = cv2.flip(frame, 1)
+    mirrored = cv2.flip(thresholded, 1)
     # --------***---------
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', mirrored)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
