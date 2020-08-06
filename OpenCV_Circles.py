0299a3fc248f0670e00890eba3d2af043d09bdcb
KenoLeon <me@k3no.com>
2020-08-06 14:27:34 -0500

    OpenCv_circles.py

--- OpenCV_Circles.py
+++ OpenCV_Circles.py
@@ -1,12 +1,28 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
-    # Change colorspace:
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
-    cv2.imshow('frame', gray)
+    rows = gray.shape[0]
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=1, maxRadius=30)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv2.circle(frame, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv2.circle(frame, center, radius, (255, 0, 255), 3)
+
+
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
