5ba1545a198396c758c0cab44791f448cfe6c665
KenoLeon <me@k3no.com>
2020-08-06 14:31:04 -0500

    OpenCv circles

--- OpenCV_Circles.py
+++ OpenCV_Circles.py
@@ -6,11 +6,15 @@ while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
+
+    # Extract regions/pixels  of interest
     rows = gray.shape[0]
+    # Find circles:
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
                                param1=100, param2=30,
                                minRadius=1, maxRadius=30)
 
+    # Found circles ? Draw on top of them
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
@@ -21,7 +25,7 @@ while(True):
             radius = i[2]
             cv2.circle(frame, center, radius, (255, 0, 255), 3)
 
-
+    # Display orignal frame:
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
